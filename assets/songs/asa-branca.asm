857a139d68aacffc33b184b1eb5f7884c752e288
devwenderson <wend.devdesigner@gmail.com>
2025-11-27 08:41:18 -0300

    add: musica

---
+++ assets/songs/asa-branca.asm
@@ -0,0 +1,303 @@
+.text
+main:	addi $4, $0, 67		# Nota Sol
+	addi $5, $0, 300
+    	addi $6, $0, 26		# Instrumento
+        addi $7, $0, 127	# Volume
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 300
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 69		# Nota lá
+	addi $5, $0, 300
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 300
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 71		# Nota si
+	addi $5, $0, 600
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 600
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 74		# Nota Ré
+	addi $5, $0, 600
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 600
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 74		# Nota Ré
+	addi $5, $0, 600
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 600
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 71		# Nota si
+	addi $5, $0, 600
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 600
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 72		# Nota Dó
+	addi $5, $0, 600
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 1200
+	addi $2, $0, 32
+	syscall
+	
+	# Segunda parte
+	addi $4, $0, 67		# Nota Sol
+	addi $5, $0, 300
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 300
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 69		# Nota lá
+	addi $5, $0, 300
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 300
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 71		# Nota si
+	addi $5, $0, 600
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 600
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 74		# Nota Ré
+	addi $5, $0, 600
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 600
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 74		# Nota Ré
+	addi $5, $0, 600
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 600
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 72		# Nota si
+	addi $5, $0, 600
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 600
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 71		# Nota Dó
+	addi $5, $0, 600
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 1200
+	addi $2, $0, 32
+	syscall
+	
+	# Terceira parte
+	addi $4, $0, 67		# Nota Sol
+	addi $5, $0, 300
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 300
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 67		# Nota Sol
+	addi $5, $0, 300
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 300
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 69		# Nota lá
+	addi $5, $0, 300
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 300
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 71		# Nota Si
+	addi $5, $0, 600
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 600
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 74		# Nota Ré
+	addi $5, $0, 600
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 1200
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 74		# Nota ré
+	addi $5, $0, 300
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 300
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 72		# Nota dó
+	addi $5, $0, 300
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 300
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 71		# Nota si
+	addi $5, $0, 300
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 300
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 67		# Nota sol
+	addi $5, $0, 600
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 600
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 72		# Nota dó
+	addi $5, $0, 1200
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 1200
+	addi $2, $0, 32
+	syscall
+	
+	# Quarta parte
+	addi $4, $0, 71		# Nota si
+	addi $5, $0, 300
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 300
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 71		# Nota si
+	addi $5, $0, 300
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 300
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 69		# Nota lá
+	addi $5, $0, 300
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 300
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 69		# Nota lá
+	addi $5, $0, 600
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 600
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 71		# Nota si
+	addi $5, $0, 600
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 600
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 69		# Nota lá
+	addi $5, $0, 300
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 300
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 69		# Nota lá
+	addi $5, $0, 300
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 300
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 67		# Nota sol
+	addi $5, $0, 300
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 300
+	addi $2, $0, 32
+	syscall
+	
+	addi $4, $0, 67		# Nota sol
+	addi $5, $0, 1200
+	addi $2, $0, 31		
+	syscall
+	
+	addi $4, $0, 1200
+	addi $2, $0, 32
+	syscall
+	
